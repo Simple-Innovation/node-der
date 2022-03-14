de66b61a1d40a08c61ce0ef34b7a72580bfb51fb
Sebastian Rogers <sebastian.rogers@simpleinnovation.co.uk>
2022-03-14 21:17:07 +0000

    Try CommonJS style import

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 import { getPrivateKeyFromBase64DER } from "./src/getPrivateKeyFromBase64DER";
 
-module.exports = { getPrivateKeyFromBase64DER };
 export { getPrivateKeyFromBase64DER };
--- src/getPrivateKeyFromBase64DER.ts
+++ src/getPrivateKeyFromBase64DER.ts
@@ -1,9 +1,11 @@
-import { asn1, pkcs12, pki, util } from "node-forge";
+// import { asn1, pkcs12, pki, util } from "node-forge";
+import pkg from "node-forge";
+const { asn1, pki, util } = pkg;
 
 export function getPrivateKeyFromBase64DER(base64der: string) {
   const der = util.decode64(base64der);
   const asn1String = asn1.fromDer(der);
-  const pkcs12Pfx: pkcs12.Pkcs12Pfx = pkcs12.pkcs12FromAsn1(asn1String);
+  const pkcs12Pfx: pkg.pkcs12.Pkcs12Pfx = pkg.pkcs12.pkcs12FromAsn1(asn1String);
   const keyBagCollection = pkcs12Pfx.getBags({
     bagType: pki.oids.pkcs8ShroudedKeyBag,
   });
