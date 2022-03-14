b7e098e04c503cba8bb8d1f3bff9aea1cd24660c
Sebastian Rogers <sebastian.rogers@simpleinnovation.co.uk>
2022-03-14 20:57:56 +0000

    Convert to module

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export * from "./src/getPrivateKeyFromBase64DER";
+import { getPrivateKeyFromBase64DER } from "./src/getPrivateKeyFromBase64DER";
+
+module.exports = { getPrivateKeyFromBase64DER };
+export { getPrivateKeyFromBase64DER };
